dd8287885abddae63609ee09a96cf977a6d967ec
Khanh Ngoc <pkngoccntt2211025@student.ctuet.edu.vn>
2025-03-14 13:48:42 +0700

    gh

--- src/components/CustomerComponents/HeaderComponent/HeaderComponent.jsx
+++ src/components/CustomerComponents/HeaderComponent/HeaderComponent.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SubHeaderComponent from "../SubHeaderComponent/SubHeaderComponent"
+import NavbarComponent from "../NavbarComponent/NavbarComponent"
 
 const HeaderComponent = () => {
-  return <div>HeaderComponent</div>;
+  return <div>
+  <SubHeaderComponent />
+  <NavbarComponent />
+  </div>;
 };
 
 export default HeaderComponent;
